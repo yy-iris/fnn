ee0763a4a3e85a168dd22d3a85848fec26599fa1
yuu <yy.iris@outlook.com>
2021-08-05 17:44:54 +0800

    modify .sh file

--- runToday.sh
+++ runToday.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
-
-nohup CUDA_VISIBLE_DEVICES=0 python lstmonly.py > lstmonly.log 2>&1 &
-nohup CUDA_VISIBLE_DEVICES=1 python lstmfnn.py > lstmfnn.log 2>&1 &
-nohup CUDA_VISIBLE_DEVICES=2 python mlp.py > mlp.log 2>&1 &
-nohup CUDA_VISIBLE_DEVICES=3 python mlpfnn.py > mlpfnn.log 2>&1 &
+mkdir -p model/mlp
+mkdir -p model/mlpfnn
+mkdir -p model/lstm
+mkdir -p model/lstmfnn
+CUDA_VISIBLE_DEVICES=0 nohup python lstmonly.py > lstmonly.log 2>&1 &
+CUDA_VISIBLE_DEVICES=1 nohup python lstmfnn.py > lstmfnn.log 2>&1 &
+CUDA_VISIBLE_DEVICES=2 nohup python mlp.py > mlp.log 2>&1 &
+CUDA_VISIBLE_DEVICES=3 nohup python mlpfnn.py > mlpfnn.log 2>&1 &
